48627e9de92ecef7e12bbf125a348d5ddf9af7c4
maithfiller <faithmiller528@outlook.com>
2020-03-04 19:58:44 -0500

    Beginning the css, html, and react code for the homepage of the game

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './phase10.jpg';
 import './App.css';
 
+
 class App extends Component {
 state = {
     data: null
@@ -24,15 +25,34 @@ state = {
     return body;
   };
 
+submitFormHandler = event => {
+  event.preventDefault();
+
+  let numplayer = console.dir(this.refs.players.value); //will give us the name value
+}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Phaze 5</h1>
-        </header>
-        <p className="App-intro">{this.state.data}</p>
+      <div className="homeBackground">
+          <img src={logo}  className="logo" alt="logo" />
+          <text className="header"> Welcome to Phaze 5</text>
+          <form className="form" onSubmit={this.submitFormHandler}>
+            <div>
+            <text className="text">How many players: </text>
+            <input type="number" min="1" max="6" name="players" ref="players" style={{width: "250px"}}/>
+            <button> Submit </button>
+            </div>
+            <button className="button"> Play Game </button>
+          </form>
+          <div>
+          <button>How to Play!</button>
+          </div>
+        <p>{this.state.data}</p>
+      <div>
+      <text className="create"> Created by: Faith Miller, Mackenzie Knight, Tristan Garcia, Eduardo Antonini, and Katie Rombeiro </text>
       </div>
+      </div>
+
     );
   }
 }
