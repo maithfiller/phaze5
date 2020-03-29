2b9999f99956204c134ba3a921922231bc3adc68
Katie Rombeiro <katierombeiro@Katies-MacBook-Pro-3.local>
2020-03-29 13:22:56 -0400

    fixed localhost connection and card images

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Routes from "./routes"
 import { Router } from "react-router-dom"
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import createHistory from 'history/createBrowserHistory'
 const history = createHistory()
