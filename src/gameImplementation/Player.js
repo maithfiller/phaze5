d22b6a7f08fee29ef20beddcd17c701fd0c0ccf7
tjgarcia0427 <42558729+tjgarcia0427@users.noreply.github.com>
2020-03-30 14:41:19 -0400

    Update Player.Js

--- src/gameImplementation/Player.js
+++ src/gameImplementation/Player.js
@@ -1,30 +1,62 @@
-import Card from './Deck';
-
 class Player {
 
   constructor(name) {
-    this.name = name;
+    this._name = name;
     // holds Player's current phase
-    this.phase = 1;
+    this._phase = 1;
     // holds Player's hand of cards
-    this.hand = [];
+    this._hand = [];
     // holds the board of Cards laid down
-    this.board1 = [];
-    this.board2 = [];
+    this._board1 = [];
+    this._board2 = [];
     // holds if the Player is skipped for a turn
     this.skipped = false;
     // player's points
-    this.points = 0;
+    this._points = 0;
+  }
+
+  /* takeTurn() function will allow a Player to
+  decide whether they wish to draw from the Deck or
+  from the discard pile. Player will then have to
+  evaluate if they wish to try to complete a phase
+  (must be their current phase). Lastly, a Player
+  will choose a card from their hand to discard */
+  takeTurn(deck, discard_pile) {
+
   }
 
   get phase() {
-    return this.phase;
+    return this._phase;
   }
-
+  set phase(p) {
+    this._phase = p;
+  }
   get name() {
-    return this.name;
+    return this._name;
   }
-
+  
+  set name(n) {
+    this._name = n;
+  }
+  
+  get hand(){
+  return this._hand;
+  }
+  set hand(h) {
+    this._hand = h;
+  }
+  get board1(){
+  return this._board1;
+  }
+  set board1(b) {
+    this._board1 = b;
+  }
+  get board2(){
+  return this._board2;
+  }
+  set board2(b) {
+    this._board2 = b;
+  }
   get isSkipped() {
     let temp = this.skipped;
     this.skipped = false;
@@ -32,45 +64,73 @@ class Player {
   }
 
   get handSize() {
-    return this.hand.length;
+    return this._hand.length;
   }
 
   get points() {
-    return this.points;
+    return this._points;
   }
-
-  get board1() {
-    return this.board1;
+  set points(p) {
+    this._points = p;
   }
-
-  get board2() {
-    return this.board2;
-  }
-
   addPhase() {
-    this.phase++;
+    this._phase++;
   }
 
-  moveCardsToBoard(cardsToMove, origin_array) {
-    cardsToMove.sort();
+  valueOf(i){
+  return this._hand[i];
+  }
+  
+  moveCardsToBoard1(cardsToMove, origin_array) {
     for (let i = cardsToMove.length - 1; i >= 0; i--) {
-      this.board1.push(origin_array.splice(cardsToMove[i], 1));
+      if(Number(cardsToMove[i]) == origin_array.length - 1){
+        this._board1.push(origin_array.pop());
+      }
+      else{
+        this._board1.push(...origin_array.splice(cardsToMove[i], 1));
+      }
     }
   }
 
-  moveCardsToBoard2(cardsToMove, origin_array) {
-    cardsToMove.sort();
-    for (let i = cardsToMove.length - 1; i >= 0; i--) {
-      this.board2.push(origin_array.splice(cardsToMove[i], 1));
+  moveCardsToBoard2(cardsToMove, boardOneIndex, origin_array) {
+    if(boardOneIndex[0] == -1){
+     for (let i = cardsToMove.length - 1; i >= 0; i--) {
+      if(Number(cardsToMove[i]) == origin_array.length - 1){
+        this._board2.push(origin_array.pop());
+      }
+      else{
+        this._board2.push(...origin_array.splice(cardsToMove[i], 1));
+      }
+    }
+    }
+    else{
+      cardsToMove.sort();
+      let newIndex = [];
+      for(let i = 0; i < cardsToMove.length; i++){
+       let counter = 0;
+        for(let k = 0; k < boardOneIndex.length; k++){
+          if(boardOneIndex[k] < Number(cardsToMove[i]))
+            counter++;
+        } 
+        newIndex.push(cardsToMove[i] - counter);
+      }
+      newIndex.sort();
+      for (let i = newIndex.length - 1; i >= 0; i--) {
+        if(newIndex[i] == origin_array.length - 1){
+         this._board2.push(origin_array.pop());
+        }
+        else{
+          this._board2.push(...origin_array.splice(newIndex[i], 1));
+        }
+     }
     }
   }
 
   isBoardEmpty() {
-    return this.board1.length == 0 && this.board2.length == 0;
+    return this._board1.length == 0 && this._board2.length == 0;
   }
-
+  
   draw(decc, discard_pile, choice) {
-
     let temp1, temp2;
 
     // if we run out of cards to draw from decc
@@ -99,11 +159,11 @@ class Player {
     temp1 = discard_pile.pop();
 
     // if temp1 is a skip
-    if (temp1.number() == 13) {
+    if (temp1._number() == 13) {
       // put temp1 back in the discard_pile
       discard_pile.push(temp1);
       // ignore user choice, user must draw from deck
-      this.hand.push(decc.dealCard());
+      this._hand.push(decc.dealCard());
       return;
     }
     // else, temp1 is not a skip
@@ -112,11 +172,11 @@ class Player {
       discard_pile.push(temp1);
       // if choice is 1, draw from deck
       if (choice == 1) {
-        this.hand.push(decc.pop());
+        this._hand.push(decc.pop());
       }
       // else, pick up from discard_pile
       else {
-        this.hand.push(discard_pile.pop());
+        this._hand.push(discard_pile.pop());
       }
     }
     // end of draw()
@@ -124,27 +184,36 @@ class Player {
 
   sortHand() {
     // selection sort performs fewer swaps than bubble sort
-    let i, j, min_idx, len = this.hand.length;
+    let i, j, min_idx, len = this._hand.length;
     let temp;
     // one by one move boundary of unsorted subarray
     for (i = 0; i < len; i++) {
       // find the minimum element in unsorted array
       min_idx = i;
       for (j = i + 1; j < len; j++)
-        if (this.hand[j].number < this.hand[min_idx].number)
+        if (this._hand[j]._number < this._hand[min_idx]._number)
           min_idx = j;
       // swap the found minimum element with the first element
-      temp = this.hand[i];
-      this.hand[i] = this.hand[min_idx];
-      this.hand[min_idx] = temp;
+      temp = this._hand[i];
+      this._hand[i] = this._hand[min_idx];
+      this._hand[min_idx] = temp;
     }
   }
+  clearBoard() {
+    // clear both boards of a player
+    this._board1 = [];
+    this._board2 = [];
+  }
 
+  clearHand() {
+    // clear a player's hand
+    this._hand = [];
+  }
   addUpPoints() {
     let number, total = 0;
 
-    for (let i = 0; i < this.hand.length; i++) {
-      number = this.hand[i].number();
+    for (let i = 0; i < this._hand.length; i++) {
+      number = this._hand[i]._number();
 
       if (number >= 1 && number <= 9) {
         total += 5;
@@ -159,55 +228,43 @@ class Player {
       }
     }
 
-    this.points += total;
+    this._points += total;
   }
 
-  clearBoard() {
-    // clear both boards of a player
-    this.board1 = [];
-    this.board2 = [];
-  }
-
-  clearHand() {
-    // clear a player's hand
-    this.hand = [];
-  }
-
-  showBoards(){
+  showBoards() {
     // board 1
-    let printStr = this.name + "'s" + " boards: ";
-    for (let i = 0; i < this.board1.length; i++) {
-      printStr += "|  "
-      printStr += this.board1[i].number
-      printStr += "  |"
+    let printStr = this._name + "'s" + " boards: ";
+    for (let i = 0; i < this._board1.length; i++) {
+      printStr += "|  ";
+      printStr += this._board1[i]._number;
+      printStr += "  |";
     }
     // board 2
     printStr += "  &  ";
-    for (let i = 0; i < this.board2.length; i++) {
-      printStr += "|  "
-      printStr += this.board2[i].number
-      printStr += "  |"
+    for (let i = 0; i < this._board2.length; i++) {
+      printStr += "|  ";
+      printStr += this._board2[i]._number;
+      printStr += "  |";
     }
-    alert(printStr)
+    alert(printStr);
   }
 
-    showHand() {
-    let printStr = this.name + "'s" + " hand: ";
-    for (let i = 0; i < this.hand.length; i++) {
-      printStr += "|  "
-      printStr += this.hand[i].number
-      printStr += "  |"
+  showHand() {
+    let printStr = this._name + "'s" + " hand: ";
+    for (let i = 0; i < this._hand.length; i++) {
+      printStr += "|  ";
+      printStr += this._hand[i]._number;
+      printStr += "  |";
     }
-    alert(printStr)
+    alert(printStr);
   }
-
+  
   makeSkipTrue() {
     this.skipped = true;
   }
-
   dropCard(index) {
     // removes one item from the hand array at specified index
-    let temp = this.hand.splice(index, 1);
+    let temp = this._hand.splice(index, 1);
     return temp;
   }
 
