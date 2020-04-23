1c6f0efe5c049a4db3ba32823961a12b250e84aa
Mackenzie  Knight <mackenzieknight@MackenziesMBP3.attlocal.net>
2020-04-22 21:18:01 -0400

    Small instruction change

--- src/components/HowToPlay.js
+++ src/components/HowToPlay.js
@@ -18,7 +18,7 @@ import './howto.css'
     <ol> Once you feel that you have finished laying down cards, choose a card to discard. </ol>
     <ol> THE GOAL OF EACH ROUND IS TO MOVE TO THE NEXT PHASE AND LAY DOWN ALL OF YOUR CARDS </ol>
     <ol> THE GOAL OF THE GAME IS TO BE THE FIRST PERSON TO GO THROUGH ALL 5 PHASES FIRST! </ol>
-    <ol> If more than one person finishes all 5 phases at the same time, then the person with the least amount of points WINS! YAY! </ol>
+    <ol> If more than one person finishes phase 2 in the same round, then the person with the least amount of points WINS! YAY! </ol>
     <ol> How do the points work? </ol>
     <ol> After every phase, the person who ran out of cards first gets 0 points, but everyone else must count how many points they have in their hand </ol>
     <ol> To do this... </ol>
